76d7d374461396be2bac3ec000aab4f09017be0b
Robert Elder <robert@robertelder.org>
2024-02-07 15:14:18 -0500

    Update to include Metallic Copper block and include dedication for Wooden block.  Previous commit hash: 5bb85557b352985bc64be7aa1559d0f07de71fb1, Former-commit-id: 9eb071608ee8183193a7efea7d35a09e74676907

--- res-modules/block-manager-single-player-client/run_single_player_client.sh
+++ res-modules/block-manager-single-player-client/run_single_player_client.sh
@@ -6,4 +6,4 @@
 mvn clean install && #  Only required when dependency packages have been updated.
 mvn -pl res-modules/block-manager-core -amd clean install &&
 mvn -pl res-modules/block-manager-single-player-client -amd clean compile package spring-boot:repackage &&
-java -jar res-modules/block-manager-single-player-client/target/block-manager-single-player-client-0.0.1.jar --log-file /tmp/single-player-block-client.log --block-world-file /tmp/single-player-world.sqlite
+java -jar res-modules/block-manager-single-player-client/target/block-manager-single-player-client-0.0.2.jar --log-file /tmp/single-player-block-client.log --block-world-file /tmp/single-player-world.sqlite
--- res-modules/block-manager-unit-tests/run_tests.sh
+++ res-modules/block-manager-unit-tests/run_tests.sh
@@ -3,4 +3,4 @@
 mvn clean install && #  Only required when dependency packages have been updated.
 mvn -pl res-modules/block-manager-core -amd clean install &&
 mvn -pl res-modules/block-manager-unit-tests -amd clean compile package spring-boot:repackage &&
-java -jar res-modules/block-manager-unit-tests/target/block-manager-unit-tests-0.0.1.jar
+java -jar res-modules/block-manager-unit-tests/target/block-manager-unit-tests-0.0.2.jar
